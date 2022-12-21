9c26836b9813f2b2ac1153fcd54fd7b83c24dd4d
ShynarZhakan <schubblezam@gmail.com>
2022-12-21 17:27:25 +0600

    hw9

---
+++ task64/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(PrintNums(n));
+
+string PrintNums(int N)
+{
+    if (N==0) return "";
+    return N + " " + PrintNums(N-1);
+}
---
+++ task68/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"A({m}, {n}) = {AccermanFunction(m,n)}");
+
+int AccermanFunction (int m, int n)
+{
+    if (m == 0)
+    return n + 1;
+  else
+    if ((m != 0) && (n == 0))
+      return AccermanFunction(m - 1, 1);
+    else
+      return AccermanFunction(m - 1, AccermanFunction(m, n - 1));
+}
